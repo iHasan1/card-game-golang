c77f02d51c8cfe27696ca701fad1ac3d1a9052d7
Imran Hasan <imran.h94@gmail.com>
2023-09-18 00:29:03 +0500

    added another test to check if file is being created and read from

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -15,5 +18,20 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d) - 1] != "Four of Clubs" {
 		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d) - 1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile (t *testing.T) {
+	os.Remove("_deckTesting")
+
+	deck := newDeck()
+	deck.saveToFile("_deckTesting")
+
+	loadedDeck := newDeckFromFile("_deckTesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_deckTesting")
 }
 
