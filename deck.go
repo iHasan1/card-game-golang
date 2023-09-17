5de9ac57586b8e35c57bcd5b6eb4e543b2b40cb3
Imran Hasan <imran.h94@gmail.com>
2023-09-17 16:28:00 +0500

    added a bit of error handling to readFile functionality

--- deck.go
+++ deck.go
@@ -45,3 +45,15 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
+
+func newDeckFromFile(filename string, ) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		// Option #1 - LOG THE ERROR AND MAKE A CALL TO NEWDECK()
+		// Option #2 - Log the error and entire quit the program
+		fmt.Println("Error: ", err) 
+		os.Exit(1)
+	}
+
+
+}
